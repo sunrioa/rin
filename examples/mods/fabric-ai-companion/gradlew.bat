e7f1409b31a2710b88ac5e170ffab1556b8cd8f5
ling <sunrioa0721@qq.com>
2026-07-28 14:18:40 +0800

    chore: 将 Minecraft Mod 迁出 Rin 仓库

--- examples/mods/fabric-ai-companion/gradlew.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-SETLOCAL
-SET APP_HOME=%~dp0
-"%JAVA_HOME%\bin\java.exe" %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%~n0" -classpath "%APP_HOME%gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
-SET EXIT_CODE=%ERRORLEVEL%
-ENDLOCAL & EXIT /B %EXIT_CODE%
